104aff146e9cb8515fe50316cf141a8acf52f6ae
Clive <clivefernandes20@gmail.com>
2020-04-11 19:53:25 +0530

    Update nlpapp.py

--- nlpapp.py
+++ nlpapp.py
@@ -221,15 +221,14 @@ if file_type == 'csv' or file_type == 'xlsx':
 
                 st.write('completed in ',int(time.time()-t1),' sec')
                 st.dataframe(df)    
+                
+                if st.button("Export File"):
+                     csv = df.to_csv(index=False)
+                     b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+                     href = f'<a href="data:file/csv;base64,{b64}" download>Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
+                     st.markdown(href, unsafe_allow_html=True)
 
-                csv = df.to_csv(index=False)
-                b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-                href = f'<a href="data:file/csv;base64,{b64}" download>Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
-                st.markdown(href, unsafe_allow_html=True)
 
-
-
-                #download the vectorizer 
 
 
 
