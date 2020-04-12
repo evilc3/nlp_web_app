2dc22279477c08c775d4aaae41da9e3617dca115
Clive <clivefernandes20@gmail.com>
2020-04-12 08:47:48 +0530

    modified file

--- nlpapp.py
+++ nlpapp.py
@@ -222,10 +222,10 @@ if file_type == 'csv' or file_type == 'xlsx':
                 st.write('completed in ',int(time.time()-t1),' sec')
                 st.dataframe(df)    
                 
-                if st.button("Export File"):
+                if st.button("download file"):
                      csv = df.to_csv(index=False)
                      b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-                     href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
+                     href = f'<a href="data:file/csv;base64,{b64} download">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
                      st.markdown(href, unsafe_allow_html=True)
 
 
