f578fd458a1f0f16b4b90338c1c70bbcea8e91b0
Clive <clivefernandes20@gmail.com>
2020-04-12 10:15:52 +0530

    del df and csv

--- nlpapp.py
+++ nlpapp.py
@@ -229,12 +229,17 @@ if file_type == 'csv' or file_type == 'xlsx':
                 #show download link 
 
                 csv = df.to_csv(index=False)
+                
+                
+                #delete df
+                del df
+                
                 b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
                 href = f'<a href="data:file/csv;base64,{b64}" download>Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
                 st.markdown(href, unsafe_allow_html=True)
 
-
-                            
+                def csv
+                               
 
 
                 
