c5924d85deb5386b2f7f78bb7805de1d9cfc0cb2
Clive <clivefernandes20@gmail.com>
2020-04-12 10:58:55 +0530

    Update nlpapp.py

--- nlpapp.py
+++ nlpapp.py
@@ -227,16 +227,17 @@ if file_type == 'csv' or file_type == 'xlsx':
 
 
                 #show download link 
+                if st.button("Download file"):
+                     csv = df.to_csv(index=False)
 
-                csv = df.to_csv(index=False)
-                
-                
-                #delete df
-                del df
-                
-                b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-                href = f'<a href="data:file/csv;base64,{b64}" download>Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
-                st.markdown(href, unsafe_allow_html=True)
+
+                     #delete df
+                     del df
+                     with st.spinner("preparing"):
+                           b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+                     del csv
+                     href = f'<a href="data:file/csv;base64,{b64}" download>Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
+                     st.markdown(href, unsafe_allow_html=True)
 
                 
                                
